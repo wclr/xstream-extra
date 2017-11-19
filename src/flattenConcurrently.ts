3fcb46d28193cb8404e7397615a12c2bc328fc43
A O <alex.oshchepkov@gmail.com>
2017-11-19 17:51:21 +0500

    move code to src

--- src/flattenConcurrently.ts
+++ src/flattenConcurrently.ts
@@ -3,7 +3,7 @@ import flattenConcurrently from 'xstream/extra/flattenConcurrently';
 
 declare module 'xstream' {
   interface Stream<T> {
-    flattenConcurrently<R, S extends Stream<R>>(this: Stream<S>): S;
+    flattenConcurrently<R, S extends Stream<R>>(this: Stream<S>): Stream<R>;
   }
 };
 
--- src/flattenSequentially.ts
+++ src/flattenSequentially.ts
@@ -3,7 +3,7 @@ import flattenSequentially from 'xstream/extra/flattenSequentially';
 
 declare module 'xstream' {
   interface Stream<T> {
-    flattenSequentially<R, S extends Stream<R>>(this: Stream<S>): S;
+    flattenSequentially<R, S extends Stream<R>>(this: Stream<S>): Stream<R>;
   }
 };
 
